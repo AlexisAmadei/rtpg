25bf7aa10830379a530566ff63c582e1729330b6
alexis <alexisamadei@gmail.com>
2025-09-29 21:42:11 +0200

    [API] feat: add matches routes in index and remove old conf for app testing

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -3,35 +3,24 @@ const { withLinks } = require("../../utils/hateoas");
 
 const v1 = Router();
 const authRouter = require("./auth");
-
-v1.use("/auth", authRouter);
+const matchesRouter = require("./matches");
 
 v1.get("/", (req, res) => {
-  res.json(
-    withLinks(
-      { version: "v1" },
-      {
-        self: { href: "/api/v1" },
-        health: { href: "/api/health" },
-        matches: { href: "/api/v1/matches" },
-        auth_register: { href: "/api/v1/auth/register", method: "POST" },
-        auth_login: { href: "/api/v1/auth/login", method: "POST" },
-      }
-    )
-  );
+    res.json(
+        withLinks(
+            { version: "v1" },
+            {
+                self: { href: "/api/v1" },
+                health: { href: "/api/health" },
+                matches: { href: "/api/v1/matches" },
+                auth_register: { href: "/api/v1/auth/register", method: "POST" },
+                auth_login: { href: "/api/v1/auth/login", method: "POST" },
+            }
+        )
+    );
 });
 
-v1.get("/matches", (req, res) => {
-  const items = [];
-  res.json(
-    withLinks(
-      { items },
-      {
-        self: { href: "/api/v1/matches" },
-        create: { href: "/api/v1/matches", method: "POST" },
-      }
-    )
-  );
-});
+v1.use("/matches", matchesRouter);
+v1.use("/auth", authRouter);
 
 module.exports = v1;
